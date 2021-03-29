00a667de70f771c0613dcb433737db85537efda3
Alejandro Veloz <alejandroveloz89@gmail.com>
2021-03-29 14:05:19 -0600

    Adding ExampleController, ExampleService, LoggerService and Interfaces.

---
+++ LvvlStarterNetApi.Api/Controllers/ExampleController.cs
@@ -0,0 +1,55 @@
+﻿using LvvlStarterNetApi.SharedKernel.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LvvlStarterNetApi.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        private readonly ILoggerService _logger;
+        private readonly IExampleService _exampleService;
+        public ExampleController(ILoggerService logger, IExampleService exampleService)
+        {
+            _logger = logger;
+            _exampleService = exampleService;
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            _logger.LogInfo();
+            _exampleService.Add();
+            return Ok();
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            _logger.LogInfo();
+            _exampleService.Get();
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Put()
+        {
+            _logger.LogInfo();
+            _exampleService.Update();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete()
+        {
+            _logger.LogInfo();
+            _exampleService.Delete();
+            return Ok();
+        }
+    }
+}
---
+++ LvvlStarterNetApi.Core/Services/ExampleService.cs
@@ -0,0 +1,32 @@
+﻿using LvvlStarterNetApi.SharedKernel.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LvvlStarterNetApi.Core.Services
+{
+    public class ExampleService : IExampleService
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
